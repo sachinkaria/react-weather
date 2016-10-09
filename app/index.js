04cf90f8db3970ecc74e76347baf3c0c273f303e
Sachin Karia <sachin.karia01@gmail.com>
2016-10-09 12:22:40 +0100

    set up api call with cross origin request error

--- app/index.js
+++ app/index.js
@@ -1,13 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var githubHelpers = require('utils/apiHelpers.js')
+var apiHelpers = require('./utils/apiHelpers')
 
 var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      weather: '',
+      temp: 0,
+      humidity: 0,
+      wind: 0
+    }
+  },
+  fetchData: function() {
+      apiHelpers.getCityInfo()
+      .then(function (response){
+        console.log(response)
+      })
+    },
+  componentWillMount: function(){
+    this.fetchData();
+  },
+
   render: function () {
     return (
-      <div>Hello ReactJS Program!</div>
+      <div>Hello</div>
     )
   }
-});
+})
+
 
 ReactDOM.render(<Weather />, document.getElementById('app'));
--- app/utils/apiHelpers.js
+++ app/utils/apiHelpers.js
@@ -1,19 +1,10 @@
 var axios = require('axios');
-
 var key = "9008e66e5785095f";
 
-function getCityInfo (){
-  return axios.get('http://api.wunderground.com/api/' + key + 'conditions/q/CA/San_Francisco.json' )
-}
-
 var helpers = {
-  getWeatherInfo: function(cities){
-    return.axios.all(player.map(function(city){
-      return getCityInfo(city)
-    })).then(function(response){
-      console.log(response);
-    })
+  getCityInfo: function (){
+    return axios.get('https://api.wunderground.com/api/' + key + 'conditions/q/CA/San_Francisco.json' )
   }
-}
+};
 
 module.exports = helpers;
