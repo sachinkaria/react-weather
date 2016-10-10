37d4ccfe4478f8fc04cc8dfd823b5c64ea18d131
Sachin Karia <sachin.karia01@gmail.com>
2016-10-10 22:47:14 +0100

    resolved cross origin issue for jsonp request

--- app/utils/apiHelpers.js
+++ app/utils/apiHelpers.js
@@ -1,4 +1,6 @@
 var axios = require('axios');
+var jsonp = require('jsonp');
+
 var key = "9008e66e5785095f";
 
 var helpers = {
@@ -6,8 +8,10 @@ var helpers = {
     return axios.get('https://api.wunderground.com/api/' + key + '/conditions/q/IN/Pune.json')
   },
   searchCity: function(){
-    return axios.get('https://autocomplete.wunderground.com/aq?query=lond' + '&format=JSON')
+    return jsonp('https://autocomplete.wunderground.com/aq?query=lond', { param: 'cb' }, function (err, data) {
+        console.log(data.RESULTS);
+    })
   }
 };
 
-module.exports = helpers;
+  module.exports = helpers;
