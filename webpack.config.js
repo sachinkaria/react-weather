4d466ff8e7760145bd77b41de00c2571e6c2a309
Sachin Karia <sachin.karia01@gmail.com>
2016-10-12 18:13:03 +0100

    test config initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,29 @@ module.exports = {
   entry: [
     './app/index.js'
   ],
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
   output: {
     path: __dirname + '/dist',
     filename: "index_bundle.js"
   },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},{
+      {test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: "babel-loader",
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+        {
         test: /masonry|imagesloaded|fizzy\-ui\-utils|desandro\-|outlayer|get\-size|doc\-ready|eventie|eventemitter/,
         loader: 'imports?define=>false&this=>window'
     }
